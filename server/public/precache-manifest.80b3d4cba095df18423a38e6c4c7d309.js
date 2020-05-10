90f236bc1b02a9eed82c473232d32722b343386b
Jiayi Xu <jx347@cornell.edu>
2020-05-10 00:58:36 -0700

    build

---
+++ server/public/precache-manifest.80b3d4cba095df18423a38e6c4c7d309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49882c04df2e3271db06a9fba627ebe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d026be34a99bce76a7b7",
+    "url": "/static/css/main.0b06190b.chunk.css"
+  },
+  {
+    "revision": "20d102150029eff0c66f",
+    "url": "/static/js/2.3ddb9a41.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/static/js/2.3ddb9a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d026be34a99bce76a7b7",
+    "url": "/static/js/main.eec05877.chunk.js"
+  },
+  {
+    "revision": "a399b3f1aef0163d85c2",
+    "url": "/static/js/runtime-main.28773b43.js"
+  }
+]);
